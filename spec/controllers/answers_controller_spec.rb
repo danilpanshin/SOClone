24b3d5494011a09f3d3ad16b19519ae92c3609ea
danilpanshin <danilpanshin@gmail.com>
2018-06-19 01:54:10 +0300

    fix answers_controller_spec.rb

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe AnswersController, type: :controller do
   let!(:user) { create(:user) }
   let!(:question) { create(:question, user: user) }
   let!(:answer) { create(:answer, question: question, user: user) }
+  let!(:some_other_user) { create(:user) }
   
   describe 'POST #create' do
     sign_in_user
@@ -40,26 +41,25 @@ RSpec.describe AnswersController, type: :controller do
            
     context 'User deletes his own answer' do      
       it 'deletes answer' do
-        expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
+        expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(-1)
       end
 
       it 'redirect to question view' do
-        delete :destroy, params: { id: answer }
+        delete :destroy, params: { question_id: question, id: answer }
         expect(response).to redirect_to question
       end
     end
 
     context 'User trying deletes another user`s answer' do
-      let!(:some_other_user) { create(:user) }
-      let!(:some_other_answer) { create(:answer, question: question, user: some_other_user) }
+      before { sign_in(some_other_user) }
 
       it 'does not deletes answer' do
-        expect { delete :destroy, params: { id: some_other_answer} }.to_not change(Answer, :count)
+        expect { delete :destroy, params: { question_id: question, id: answer } }.to_not change(Answer, :count)
       end
 
-      it 'redirect to question view' do
-        delete :destroy, params: { id: answer }
-        expect(response).to redirect_to question_path(question)
+      it 'redirect to questions view' do
+        delete :destroy, params: { question_id: question, id: answer }
+        expect(response).to redirect_to questions_path
       end
     end
   end
